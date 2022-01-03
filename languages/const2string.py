737b2d5f07af16d7184d60072c41bfe8074bc9d3
EillesWan <W-YI_DoctorYI@outlook.com>
2022-01-03 14:55:35 +0800

    增加一个文件，等会再处理，先去上课了，作业一个字都没写

---
+++ languages/const2string.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+
+
+# W-YI 金羿
+# QQ 2647547478
+# 音·创 开发交流群 861684859
+# Email EillesWan2006@163.com W-YI_DoctorYI@outlook.com EillesWan@outlook.com
+# 版权所有 Team-Ryoun 金羿
+# 若需转载或借鉴 请附作者
+
+
+#  代码写的并非十分的漂亮，还请大佬多多包涵；本软件源代码依照Apache软件协议公开
+
+'''
+将程序中用双引号""括起来的字符串
+转为字符串列表 list[str, str, ...]
+方便进行语言翻译支持。
+'''
+
+def __main__():
+    pass
+
+
+if __name__ == '__main__':
+    __main__
