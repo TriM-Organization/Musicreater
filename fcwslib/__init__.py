7e13b22d46af5e80b6e3fe91d5a50caaf2e1163f
bgArray <474037765@qq.com>
2022-01-19 13:32:53 +0800

    除个别外语法错误整体修正

--- fcwslib/__init__.py
+++ fcwslib/__init__.py
@@ -1,105 +1,114 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# 诸葛亮与八卦阵帮忙修改语法 日期：---2022年1月19日
+# 统计：致命（三级）错误：2个---未解决；警告（二级）错误：2个；语法（一级）错误：17个
+
 
 __version__ = '0.0.1'
 __all__ = ['run_server', 'subscribe', 'unsubscribe', 'send_command', 'tellraw']
 __author__ = 'Fuckcraft <https://gitee.com/fuckcraft>'
 
-import os
+# import os
 import json
 import uuid
-import logging
+# import logging
 import asyncio
 import time
 import websockets
 
+
 # 写这段代码的时候，只有我和上帝知道这段代码是干什么的。
 # 现在只有上帝知道。
+# ----
+# 没毛病，我讨厌两种人：一种是要我写注释的人，一种是给我代码看但没有写注释的人。
 
 # 此函数用于向 Minecraft 订阅请求
 async def subscribe(websocket, event_name):
-    '''
+    """
     参数:
     : websocket  : websocket 对象 :
     : event_name : 需要订阅的请求 :
 
     返回:
     None
-    '''
+    """
 
     response = {
-            'body': {
-                'eventName': str(event_name)  # 示例：PlayerMessage
-            },
-            'header': {
-                'requestId': str(uuid.uuid4()),
-                'messagePurpose': 'subscribe',
-                'version': 1,
-                'messageType': 'commandRequest'
-            }
+        'body': {
+            'eventName': str(event_name)  # 示例：PlayerMessage
+        },
+        'header': {
+            'requestId': str(uuid.uuid4()),
+            'messagePurpose': 'subscribe',
+            'version': 1,
+            'messageType': 'commandRequest'
         }
+    }
 
     # 增加 json 的可读性
     # response = json.dumps(response, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
     response = json.dumps(response)
-    
+
     await websocket.send(response)
+
 
 # 此函数用于向 Minecraft 消除订阅请求
 async def unsubscribe(webscket):
-    '''
+    """
     参数:
     : websocket  : websocket 对象     :
     : event_name : 需要消除订阅的请求 :
 
     返回:
     None
-    '''
+    """
+    print(webscket)
 
     response = {
-            "body": {
-                "eventName": str(event_name)  # PlayerMessage
-            },
-            "header": {
-                "requestId": str(uuid.uuid4()),
-                "messagePurpose": "unsubscribe",
-                "version": 1,
-                "messageType": "commandRequest"
-            }
+        "body": {
+            "eventName": str(event_name)  # PlayerMessage
+        },
+        "header": {
+            "requestId": str(uuid.uuid4()),
+            "messagePurpose": "unsubscribe",
+            "version": 1,
+            "messageType": "commandRequest"
         }
-    
+    }
+
     # 增加 json 的可读性
     # response = json.dumps(response, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
     response = json.dumps(response)
 
     await websocket.send(response)
 
+
 # 此函数用于向 Minecraft 执行命令
 async def send_command(websocket, command):
-    '''
+    """
     参数:
     : websocket : websocket 对象 :
     : command   : 执行的命令     :
 
     返回:
     None
-    '''
+    """
 
     response = {
-            'body': {
-                'origin': {
-                    'type': 'player'
-                },
-                'commandLine': str(command),
-                'version': 1
+        'body': {
+            'origin': {
+                'type': 'player'
             },
-            'header': {
-                'requestId': str(uuid.uuid4()),
-                'messagePurpose': 'commandRequest',
-                'version': 1,
-                'messageType': 'commandRequest'
-            }
+            'commandLine': str(command),
+            'version': 1
+        },
+        'header': {
+            'requestId': str(uuid.uuid4()),
+            'messagePurpose': 'commandRequest',
+            'version': 1,
+            'messageType': 'commandRequest'
         }
+    }
 
     # 增加 json 的可读性
     # response = json.dumps(response, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
@@ -107,24 +116,25 @@ async def send_command(websocket, command):
 
     await websocket.send(response)
 
+
 # 此函数用于向 Minecraft 发送消息
 async def tellraw(websocket, message):
-    '''
+    """
     参数:
     : websocket : websocket 对象 :
     : message   : 发送的消息     :
 
     返回:
     None
-    '''
+    """
 
     command = {
-            'rawtext':[
-                {
-                    'text':'[{}] {}'.format(time.asctime(), message)
-                }
-            ]
-        }
+        'rawtext': [
+            {
+                'text': '[{}] {}'.format(time.asctime(), message)
+            }
+        ]
+    }
 
     # 增加 json 可读性
     # command = json.dumps(command, sort_keys=True, indent=4, separators=(', ', ': '), ensure_ascii=False)
@@ -133,9 +143,9 @@ async def tellraw(websocket, message):
 
     await send_command(websocket, command)
 
+
 def run_server(function):
     # 修改 ip 地址和端口
     start_server = websockets.serve(function, 'localhost', 8080)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
-
--- languages/const2string.py
+++ languages/const2string.py
@@ -9,7 +9,7 @@
 # 若需转载或借鉴 请附作者
 
 
-'''
+"""
    Copyright 2022 Eilles Wan (金羿)
 
    Licensed under the Apache License, Version 2.0 (the 'License')
@@ -23,21 +23,33 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-'''
-
+"""
 
 #  代码写的并非十分的漂亮，还请大佬多多包涵；本软件源代码依照Apache软件协议公开
 
-'''
-将程序中用双引号""括起来的字符串
-转为字符串列表 list[str, str, ...]
-方便进行语言翻译支持。
-'''
+# -----------------------------分割线-----------------------------
+# 诸葛亮与八卦阵帮忙修改语法 日期：---2022年1月19日
+# 统计：致命（三级）错误：0个；警告（二级）错误：0个；语法（一级）错误：12个
+# 目前我的Pycharm并没有显示任何错误，有错误可以向：
+# bgArray 诸葛亮与八卦阵
+# QQ 474037765 或最好加入：音·创 开发交流群 861684859
+# ------------------------- split line-----------------------------
+# Zhuge Liang and Bagua array help to modify the grammar date: -- January 19, 2022
+# Statistics: fatal (Level 3) errors: 0; Warning (Level 2) errors: 15; Syntax (Level 1) error: 597
+# At present, my Pycham does not display any errors. If there are errors, you can report them to me
+# Bgarray Zhuge Liang and Bagua array
+# QQ 474037765 or better join: Musicreater development exchange group 861684859
+# ------------------------- split line-----------------------------
 
-startWith = 0
+# 下面为正文
 
+
+# 将程序中用双引号""括起来的字符串
+# 转为字符串列表 list[str, str, ...]
+# 方便进行语言翻译支持。
 
 import sys
+startWith = 0
 
 
 def __main__():
@@ -46,23 +58,25 @@ def __main__():
         print('读取文件: {}'.format(fileName))
         fileText = []
         for line in open(fileName, 'r', encoding='utf-8'):
-            while line.count('"') >=2:
+            while line.count('"') >= 2:
                 # 只有上帝看得懂我在写什么。
-                if line[line.index('"'):2+line[line.index('"')+1:].index('"')+len(line[:line.index('"')])] in textList:
-                    thisText = textList.index(line[line.index('"'):2+line[line.index('"')+1:].index('"')+len(line[:line.index('"')])])
+                if line[
+                   line.index('"'):2 + line[line.index('"') + 1:].index('"') + len(line[:line.index('"')])] in textList:
+                    thisText = textList.index(
+                        line[line.index('"'):2 + line[line.index('"') + 1:].index('"') + len(line[:line.index('"')])])
                 else:
                     thisText = len(textList)
-                    textList.append(line[line.index('"'):2+line[line.index('"')+1:].index('"')+len(line[:line.index('"')])])
+                    textList.append(
+                        line[line.index('"'):2 + line[line.index('"') + 1:].index('"') + len(line[:line.index('"')])])
                 line = line.replace(
-                    line[line.index('"'):2+line[line.index('"') + 1:].index('"')+len(line[:line.index('"')])],
-                    'READABLETEXT[{}]'.format(thisText+startWith)
+                    line[line.index('"'):2 + line[line.index('"') + 1:].index('"') + len(line[:line.index('"')])],
+                    'READABLETEXT[{}]'.format(thisText + startWith)
                 )
             fileText.append(line)
-            
-        open(fileName+'_C','w',encoding='utf-8').writelines(fileText)
-        
-    
-    outFile = open('lang__.py','w',encoding='utf-8')
+
+        open(fileName + '_C', 'w', encoding='utf-8').writelines(fileText)
+
+    outFile = open('lang__.py', 'w', encoding='utf-8')
     outFile.write('''# -*- coding:utf-8 -*-
 
 # 由金羿翻译工具生成字符串列表
@@ -73,7 +87,7 @@ def __main__():
 READABLETEXT = {
 ''')
     for i in range(len(textList)):
-        outFile.write("    {}:{},\n".format(i+startWith,textList[i]))
+        outFile.write("    {}:{},\n".format(i + startWith, textList[i]))
     outFile.write('}')
     outFile.close()
 
