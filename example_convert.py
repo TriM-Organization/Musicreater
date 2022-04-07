8f6cc047809f254fb65aa5a4e4a0937be470daf3
EillesWan <W-YI_DoctorYI@outlook.com>
2022-04-07 08:27:43 +0800

    重设样例代码

--- example_convert.py
+++ example_convert.py
@@ -6,4 +6,4 @@
 
 from main import *
 
-midiConvert(input('请输入midi文件路径：'), input('请输入输出路径：')).tomcpack(1,'mscplay',1.0,1.0)
+midiConvert(input('请输入midi文件路径：'), input('请输入输出路径：')).tomcpack(1,input('请输入计分板名称：'),float(input('请输入音量（0-1）：')),float(input('请输入速度倍率：')))
