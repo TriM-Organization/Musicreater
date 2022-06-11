1fde8ca2535d590f09956f00adc54327cbbdc247
CaryXiong <a1816101108@outlook.com>
2022-06-11 12:19:15 +0800

    小改动

--- 补全库.py
+++ 补全库.py
@@ -1,2 +1,7 @@
-from msctLib.buildIN import version
-print(version().__call__())
+
+from os import system
+
+if platform == "linux":
+    system("python3 -m pip install -r ./Musicreater/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/")
+elif platform == "win32":
+    system("pip install -r ./Musicreater/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/")
