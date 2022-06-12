4b53938adf1fbcceb45bff38f2bf127acec8727e
EillesWan <EillesWan@outlook.com>
2022-06-12 18:38:31 +0800

    装逼

--- 作者的自我装逼.py
+++ 作者的自我装逼.py
@@ -1,4 +1,30 @@
 
+from time import sleep
 from msctLib.buildIN import author_reader
+from os import system
 
-input(author_reader())
+authors = author_reader()
+
+def run(string):
+    try:
+        system(string)
+    except:
+        return
+
+run('color 70')
+
+for bigname,languages in authors.items():
+    run('color 02') 
+    for i in bigname:
+        print(i,end="",flush=True)
+        sleep(0.1)
+    run('color 70')
+    print(' :')
+    for language,author in languages.items():
+        line = '  '+language+' > '+author
+        for i in line:
+            print(i,end="",flush=True)
+            sleep(0.05)
+        print()
+
+input()
