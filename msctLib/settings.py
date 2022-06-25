d88e50819dff1979dc14f648ce9a3687f48fff22
EillesWan <EillesWan@outlook.com>
2022-06-25 22:06:19 +0800

    更了？没更

--- msctLib/settings.py
+++ msctLib/settings.py
@@ -2,13 +2,20 @@
 
 
 DEFAULTBLUE = (0, 137, 242)
+# 0089F2
 WEAKBLUE = (0, 161, 231)
+# 00A1E7
 LIGHTBLUE = (38, 226, 255)
+# 26E2FF
 RED = (255, 52, 50)
+# FF3432
 PURPLE = (171, 112, 255)
+# AB70FF
 GREEN = (0, 255, 33)
 WHITE = (242, 244, 246)
+# F2F4F6
 BLACK = (18, 17, 16)
+# 121110
 
 settings = {
     'language' : 'zh-CN',
