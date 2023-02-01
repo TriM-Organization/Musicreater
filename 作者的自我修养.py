75f94dab4c616128a312e60449ca8cda3c628f14
EillesWan <W-YI_DoctorYI@outlook.com>
2023-02-01 18:24:44 +0800

    准备与pkgver合并

--- 作者的自我修养.py
+++
@@ -1,8 +0,0 @@
-
-from os import system
-from sys import platform
-
-if platform == "linux":
-    system("python3 -m pip install -r ./requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/")
-elif platform == "win32":
-    system("pip install -r ./requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/")
