bf173a9f81ed2488b156769a653c4de94d91e9f6
Eilles <EillesWan@outlook.com>
2026-01-08 03:18:20 +0800

    没有变化

--- example_singleConvert.py
+++ example_singleConvert.py
@@ -1,11 +1,13 @@
 import Musicreater
 import Musicreater.plugin
-import Musicreater.plugin.addonpack
+import Musicreater.plugin.mcstructfile
 
 print(
-    Musicreater.plugin.addonpack.to_addon_pack_in_repeater_divided_by_instrument(
+    Musicreater.plugin.mcstructfile.to_mcstructure_file_in_delay(
         Musicreater.MidiConvert.from_midi_file(
-            input("midi路径:"), old_exe_format=False
+            input("midi路径:"),
+            old_exe_format=False,
+            # note_table_replacement={"note.harp": "note.flute"},
         ),
         input("输出路径:"),
         # Musicreater.plugin.ConvertConfig(input("输出路径:"),),
